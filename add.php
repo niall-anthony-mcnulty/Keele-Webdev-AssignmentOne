79abd128d30798fd9cbf47d6ffbb2a8af5132303
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-10-02 16:39:52 +0800

    setting up backbone

--- add.php
+++ add.php
@@ -1,8 +1,7 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
@@ -10,17 +9,15 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Sora:wght@800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/add.css">
+    <link rel='stylesheet' href='css/nav.css'>
     <link rel="icon" href="images/data.png">
-    <title>Staff Database</title>
+    <title>Add Staff Member</title>
 </head>
 <body>
-    <header class='nav'>
-        <?php include_once 'includes/nav.php'?>
+    <header>
+        <?php include_once 'includes/nav.php'?>  
     </header>
-
+    
 </body>
-
 </html>
-
---
+++ includes/nav.php
@@ -0,0 +1,6 @@
+
+<nav class='topnav'>
+    <a class='home' href='index.php'>Home</a>
+    <a href='add.php'>Add Staff</a>
+</nav>
+
